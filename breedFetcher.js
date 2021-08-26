9673be87a72257edc6c01f56d67c5e519f1c8735
Teenaelza <teena.thaliath@gmail.com>
2021-08-26 21:07:31 +0000

    added the edge cases to print out the error

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,21 @@
 const request = require("request");
+const breed = process.argv.slice(2);
+if (breed[0].length === 0) {
+  console.log("Enter a breed name as an argument");
+  process.exit();
+}
 request(
-  "https://api.thecatapi.com/v1/breeds/search?q=sib",
+  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
   (error, response, body) => {
-    console.log(body);
-    console.log(typeof body);
+    //console.log(response.statusCode);
+    const BreedObject = JSON.parse(body);
+    console.log(BreedObject[0].description);
+    if (BreedObject.length === 0) {
+      console.log(`The ${breed} not found`);
+    }
+    if (error) {
+      console.log("error:", error); // Print the error if one occurred
+      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response
+    }
   }
 );
