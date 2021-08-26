edef9142872f2c76c993a8ce63dddb66d767b11c
Teenaelza <teena.thaliath@gmail.com>
2021-08-26 23:47:01 +0000

    implemented mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,16 @@
 const request = require("request");
 const fetchBreedDescription = function(breed, callback) {
   request(
-    `https://api.thectapi.com/v1/breeds/search?q=${breed}`,
+    `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
     (error, response, body) => {
-      callback(error, body);
+      const BreedObject = JSON.parse(body);
+      let desc = "";
+      if (BreedObject.length > 0) {
+        desc = BreedObject[0].description;
+      } else {
+        desc = "The Breed not found";
+      }
+      callback(error, desc);
     }
   );
 };
