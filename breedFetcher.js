d0cb0b9e72e5cf0cc92e35925048563d39685f10
Teenaelza <teena.thaliath@gmail.com>
2021-08-26 23:20:13 +0000

    refactored the files using call back

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,10 @@
 const request = require("request");
-const breed = process.argv.slice(2);
-if (breed[0].length === 0) {
-  console.log("Enter a breed name as an argument");
-  process.exit();
-}
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  (error, response, body) => {
-    //console.log(response.statusCode);
-    const BreedObject = JSON.parse(body);
-    console.log(BreedObject[0].description);
-    if (BreedObject.length === 0) {
-      console.log(`The ${breed} not found`);
+const fetchBreedDescription = function(breed, callback) {
+  request(
+    `https://api.thectapi.com/v1/breeds/search?q=${breed}`,
+    (error, response, body) => {
+      callback(error, body);
     }
-    if (error) {
-      console.log("error:", error); // Print the error if one occurred
-      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response
-    }
-  }
-);
+  );
+};
+module.exports = { fetchBreedDescription };
